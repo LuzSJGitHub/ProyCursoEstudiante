006a7c9553e9de36e5d5165252ba8c9a70ddeeed
Luz2021 <33913286+LuzSJGitHub@users.noreply.github.com>
2021-10-23 14:30:38 +0000

    incluyendo relaciones de curso

--- app/models/estudiante.rb
+++ app/models/estudiante.rb
@@ -1,7 +1,9 @@
 class Estudiante < ApplicationRecord
   belongs_to :curso
-
   validates :app_paterno,
-    :app_materno, presence: { message: ": Este campo es requerido" }
+    :app_materno, 
+    presence: { 
+      message: ": Este campo es requerido" 
+    }
 
 end
